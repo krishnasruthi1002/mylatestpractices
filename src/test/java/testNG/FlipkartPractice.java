7ce4aa3e010e21f522d2e9ceafa7fd5c0f652bb7
sruthi <krishnasruthi0604@gmail.com>
2021-11-13 10:19:52 +0530

    flipkartclass added

--- src/test/java/testNG/FlipkartPractice.java
+++ src/test/java/testNG/FlipkartPractice.java
@@ -1,5 +1,26 @@
 package testNG;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
 public class FlipkartPractice {
+	WebDriver driver;
+	
+	@Parameters({ "URL" })
+	@BeforeTest
+	public void logintoHomePage(String urlname) {
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium workspace\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(urlname);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 }
+
+}
+	
